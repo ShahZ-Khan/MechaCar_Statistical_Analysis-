79befe79f91f3ae05f2f5ef14c7ccd899cce17e2
ShahZ-Khan <114197558+ShahZ-Khan@users.noreply.github.com>
2023-03-03 00:39:11 -0500

    files

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -3,5 +3,29 @@ MechaCar_mpg <- read.csv("/Users/szk/Desktop/Analysis Projects/MechaCar_Statisti
  )
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
      ground_clearance + AWD, data=MechaCar_mpg)
+
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
-             ground_clearance + AWD, data=MechaCar_mpg))
+ground_clearance + AWD, data=MechaCar_mpg))
+
+SuspensionCoil <- read.csv("/Users/szk/Desktop/Analysis Projects/MechaCar_Statistical_Analysis-/Suspension_Coil.csv")
+
+total_summary <- SuspensionCoil %>% summarize(Mean = mean(PSI), 
+                                              Median = median(PSI), 
+                                              Variance = var(PSI),
+                                              SD = sd(PSI))
+
+lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>%
+  summarize(Mean = mean(PSI), 
+            Median = median(PSI), 
+            Variance = var(PSI),
+            SD = sd(PSI), .groups= 'keep')
+
+t.test(SuspensionCoil$PSI, mu=1500)
+
+
+t.test(subset(SuspensionCoil, Manufacturing_Lot == "Lot1")$PSI, mu = 1500)
+
+t.test(subset(SuspensionCoil, Manufacturing_Lot == "Lot2")$PSI, mu = 1500)
+
+t.test(subset(SuspensionCoil, Manufacturing_Lot == "Lot3")$PSI, mu = 1500)
+
